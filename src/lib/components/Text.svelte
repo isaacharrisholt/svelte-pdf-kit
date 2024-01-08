d2bb9e828f8f036bcfa86266adfa8593c2091e50
Isaac Harris-Holt <isaac@harris-holt.com>
2024-01-08 19:23:08 +0000

    document text props

--- src/lib/components/Text.svelte
+++ src/lib/components/Text.svelte
@@ -3,40 +3,129 @@
 	import { FONT_WEIGHTS } from '$lib/types'
 
 	// Custom options
+	/**
+	 * The font to use for text.
+	 */
 	export let font: string | null = null
+	/**
+	 * The font style to use for text.
+	 */
 	export let style: FontStyle | null = null
+	/**
+	 * The font weight to use for text.
+	 */
 	export let weight: FontWeight | null = null
 
 	// PDFKit options
+	/**
+	 * The alignment of the text.
+	 */
 	export let align: TextOptions['align'] = undefined
+	/**
+	 * The baseline of the text.
+	 */
 	export let baseline: TextOptions['baseline'] = undefined
+	/**
+	 * The indent of the bullet.
+	 */
 	export let bulletIndent: TextOptions['bulletIndent'] = undefined
+	/**
+	 * The radius of the bullet.
+	 */
 	export let bulletRadius: TextOptions['bulletRadius'] = undefined
+	/**
+	 * The character spacing of the text.
+	 */
 	export let characterSpacing: TextOptions['characterSpacing'] = undefined
+	/**
+	 * The gap between columns.
+	 */
 	export let columnGap: TextOptions['columnGap'] = undefined
+	/**
+	 * The number of columns.
+	 */
 	export let columns: TextOptions['columns'] = undefined
+	/**
+	 * Whether the text will be followed immediately in the same segment.
+	 * Useful for changing text styling in the middle of a paragraph.
+	 */
 	export let continued: TextOptions['continued'] = undefined
+	/**
+	 * Create an anchor to a named destination.
+	 */
 	export let destination: TextOptions['destination'] = undefined
+	/**
+	 * The OpenType features to apply to the text.
+	 */
 	export let features: TextOptions['features'] = undefined
+	/**
+	 * Whether to fill the text.
+	 */
 	export let fill: TextOptions['fill'] = undefined
+	/**
+	 * The ellipsis character to use when truncating text.
+	 * Set to `true` to use the default ellipsis character.
+	 */
 	export let ellipsis: TextOptions['ellipsis'] = undefined
+	/**
+	 * Go to anchor (shortcut for creating annotations)
+	 */
 	export let goTo: TextOptions['goTo'] = undefined
+	/**
+	 * The height of the text.
+	 */
 	export let height: TextOptions['height'] = undefined
+	/**
+	 * The indent of the text.
+	 */
 	export let indent: TextOptions['indent'] = undefined
+	/**
+	 * Whether to wrap text. Set to `false` to disable wrapping on line breaks.
+	 */
 	export let lineBreak: TextOptions['lineBreak'] = undefined
+	/**
+	 * The gap between lines.
+	 */
 	export let lineGap: TextOptions['lineGap'] = undefined
+	/**
+	 * A URL to link to (shortcut for creating annotations)
+	 */
 	export let link: TextOptions['link'] = undefined
+	/**
+	 * The type of list to use.
+	 */
 	export let listType: TextOptions['listType'] = undefined
+	/**
+	 * Whether to use an oblique font. Set to `true` or specify an angle in degrees.
+	 */
 	export let oblique: TextOptions['oblique'] = undefined
+	/**
+	 * The gap between paragraphs.
+	 */
 	export let paragraphGap: TextOptions['paragraphGap'] = undefined
+	/**
+	 * Whether to strike the text.
+	 */
 	export let strike: TextOptions['strike'] = undefined
+	/**
+	 * Whether to stroke the text.
+	 */
 	export let stroke: TextOptions['stroke'] = undefined
-	export let structParent: TextOptions['structParent'] = undefined
-	export let structType: TextOptions['structType'] = undefined
-	export let structTypes: TextOptions['structTypes'] = undefined
+	/**
+	 * The indent of text in lists.
+	 */
 	export let textIndent: TextOptions['textIndent'] = undefined
+	/**
+	 * Whether to underline the text.
+	 */
 	export let underline: TextOptions['underline'] = undefined
+	/**
+	 * The width the text should be wrapped to.
+	 */
 	export let width: TextOptions['width'] = undefined
+	/**
+	 * The space between words.
+	 */
 	export let wordSpacing: TextOptions['wordSpacing'] = undefined
 
 	$: options = {
@@ -63,9 +152,6 @@
 		paragraphGap,
 		strike,
 		stroke,
-		structParent,
-		structType,
-		structTypes,
 		textIndent,
 		underline,
 		width,
