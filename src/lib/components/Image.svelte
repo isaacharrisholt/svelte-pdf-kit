805617d02fb437a194334949131f98cf4488b4e7
Isaac Harris-Holt <isaac@harris-holt.com>
2024-01-08 21:43:53 +0000

    document image props

--- src/lib/components/Image.svelte
+++ src/lib/components/Image.svelte
@@ -2,7 +2,13 @@
 	import type { ImageOptions } from '$lib/types'
 
 	// Custom options
+	/**
+	 * The source of the image. Will check the filesystem first, then the web.
+	 */
 	export let src: string
+	/**
+	 * The alternative text for the image.
+	 */
 	export let alt: string
 	/**
 	 * Display this element in this location on every page.
@@ -11,15 +17,45 @@
 	// export let fixed: ImageOptions['fixed'] = undefined
 
 	// PDFKit options
+	/**
+	 * The alignment of the image horizontally.
+	 */
 	export let align: ImageOptions['align'] = undefined
+	/**
+	 * A URL to link this image to.
+	 */
 	export let link: ImageOptions['link'] = undefined
+	/**
+	 * A named destination to link this image to.
+	 */
 	export let destination: ImageOptions['destination'] = undefined
+	/**
+	 * Image is scaled to cover the rectangle provided.
+	 */
 	export let cover: ImageOptions['cover'] = undefined
+	/**
+	 * Image is scaled to fit the rectangle provided.
+	 */
 	export let fit: ImageOptions['fit'] = undefined
+	/**
+	 * A named destination to link this image to.
+	 */
 	export let goTo: ImageOptions['goTo'] = undefined
+	/**
+	 * The height of the image.
+	 */
 	export let height: ImageOptions['height'] = undefined
+	/**
+	 * A factor by which to scale the image.
+	 */
 	export let scale: ImageOptions['scale'] = undefined
+	/**
+	 * The alignment of the image vertically.
+	 */
 	export let valign: ImageOptions['valign'] = undefined
+	/**
+	 * The width of the image.
+	 */
 	export let width: ImageOptions['width'] = undefined
 
 	$: options = {
